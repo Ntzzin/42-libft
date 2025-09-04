264a164bafea62dbf266a53960285777448988d1
Ntzzin <nytgrab@gmail.com>
2025-09-04 15:32:28 +0100

    aesthetic change...

--- srcs/ft_string/ft_memcpy.c
+++ srcs/ft_string/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: nado-nas <nado-nas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 17:09:03 by nado-nas          #+#    #+#             */
-/*   Updated: 2025/08/29 17:42:01 by nado-nas         ###   ########.fr       */
+/*   Updated: 2025/09/01 14:47:20 by nado-nas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*pd;
 	unsigned char	*ps;
-	size_t			i;
 
 	if (!dest && !src)
 		return (NULL);
 	pd = (unsigned char *)dest;
 	ps = (unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		pd[i] = ps[i];
-		i++;
-	}
+	while (n--)
+		pd[n] = ps[n];
 	return ((void *)pd);
 }
